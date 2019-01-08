86066cc71985b524d53dfe41c4cc7797b58a68de
marsaf <frederic.marsaud@laposte.net>
2019-01-08 18:26:05 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-chartify="$WERCKER_STEP_ROOT/go/bin/chartify"
+#chartify="$WERCKER_STEP_ROOT/go/bin/chartify"
+helm="$WERCKER_STEP_ROOT/helm"
 kubectl="$WERCKER_STEP_ROOT/kubectl"
-
-display_kubectl_version() {
-  info "Running kubectl version:"
-  "$kubectl" version --client
-  echo ""
-}
 
 display_chartify_version() {
   info "Running chartify version:"
@@ -15,25 +10,15 @@ display_chartify_version() {
   echo ""
 }
 
-# this should not be needed for the published step, but is needed for testing the unpublished step
-pull_helm() {
-    helm_version=2.11.0
-    helm_archive=helm-v${helm_version}-linux-amd64.tar.gz
-    helm_url=https://storage.googleapis.com/kubernetes-helm/${helm_archive}
-    echo "About to pull helm"
-    curl -L $helm_url > $helm_archive
-    echo "About to extract helm"
-    tar xvzf ${helm_archive}
-    echo "just did -  tar xzf ${helm_archive}"
-    echo "showing listing of linux-amd64"
-    ls linux-amd64
-    echo "moving helm client from $PWD to $WERCKER_STEP_ROOT"
-    mv linux-amd64/helm "$WERCKER_STEP_ROOT/"
-
-    echo "Test Helm"
-    ${WERCKER_STEP_ROOT}/helm version --client
-
+display_helm_kubectl_version() {
+  info "Running kubectl version:"
+  "$kubectl" version --client
+  echo ""
+  info "Running helm version:"
+  "$helm" version --client
+  echo ""
 }
+
 # This is used to create a kubeconfig file if the user passed server+token
 # This is also used by helm calls
 generate_kubeconfig() {
@@ -66,25 +51,93 @@ generate_kubeconfig() {
 }
 
 main() {
-  display_kubectl_version
+  display_helm_kubectl_version
   display_chartify_version
-  pull_helm
   
   kubeconfig="$KUBECONFIG_TEXT"
   server="$WERCKER_STEP_AURA_SERVER"
   token="$WERCKER_STEP_AURA_TOKEN"
-  cmd="cluster-info"
   chartify_cmd="$WERCKER_HELM_CHART_GENERATE_COMMAND"
-  
-  # echo "INFO: WERCKER_STEP_AURA_SERVER - $WERCKER_STEP_AURA_SERVER"
-  # echo "INFO: WERCKER_STEP_AURA_TOKEN - $WERCKER_STEP_AURA_TOKEN"
-  # echo "INFO: PULL_DEPENDENCIES - $PULL_DEPENDENCIES"
-  # echo "INFO: INSTALL_TYPE - $INSTALL_TYPE"
-  # echo "INFO: KUBECONFIG_TEXT - $KUBECONFIG_TEXT"
-  
+  cmd="cluster-info"
+  helm_cmd="$WERCKER_HELM_COMMAND"
+
+  helm_args=
+  # release-name
+  if [ $helm_cmd == "install" ]; then 
+    if [ -n "$WERCKER_HELM_RELEASE_NAME" ]; then
+      helm_args="$helm_args --name=\"$WERCKER_HELM_RELEASE_NAME\""
+    fi
+    # release-namespace
+    if [ -n "$WERCKER_HELM_RELEASE_NAMESPACE" ]; then
+      helm_args="$helm_args --namespace=\"$WERCKER_HELM_RELEASE_NAMESPACE\""
+    fi
+  else
+    if [ -n "$WERCKER_HELM_RELEASE_NAME" ]; then
+      helm_args="$helm_args \"$WERCKER_HELM_RELEASE_NAME\""
+    fi
+  fi
+
+  # repo
+  if [ -n "$WERCKER_HELM_REPO" ]; then
+    helm_args="$helm_args --repo=\"$WERCKER_HELM_REPO\""
+    helm_args="$helm_args \"$WERCKER_HELM_CHART_NAME\""
+  else
+    if [ -n "$WERCKER_HELM_CHART_NAME" ]; then
+      helm_args="$helm_args \"$WERCKER_HELM_CHART_NAME\""
+      $WERCKER_STEP_ROOT/envsubst < "$WERCKER_HELM_CHART_NAME/values.yaml" > "$HOME/values.yaml"
+      helm_args="$helm_args -f \"$HOME/values.yaml\""
+      cat $HOME/values.yaml
+    fi 
+  fi
+
+ # values file
+  if [ -n "$WERCKER_HELM_VALUES_FILE" ]; then
+    $WERCKER_STEP_ROOT/envsubst < "$WERCKER_HELM_VALUES_FILE" > "$HOME/values.yaml"
+    helm_args="$helm_args -f \"$HOME/values.yaml\""
+fi 
   # Global args
   global_args=
   raw_global_args="$WERCKER_HELM_CHART_GENERATE_RAW_GLOBAL_ARGS"
+  
+  # token
+  if [ -n "$WERCKER_HELM_TOKEN" ]; then
+    global_args="$global_args --token=\"$WERCKER_HELM_TOKEN\""
+  fi
+
+  # username
+  if [ -n "$WERCKER_HELM_USERNAME" ]; then
+    global_args="$global_args --username=\"$WERCKER_HELM_USERNAME\""
+  fi
+
+  # password
+  if [ -n "$WERCKER_HELM_PASSWORD" ]; then
+    global_args="$global_args --password=\"$WERCKER_HELM_PASSWORD\""
+  fi
+
+  # server
+  if [ -n "$WERCKER_HELM_SERVER" ]; then
+    global_args="$global_args --server=\"$WERCKER_HELM_SERVER\""
+  fi
+  
+  # insecure-skip-tls-verify
+  if [ -n "$WERCKER_HELM_INSECURE_SKIP_TLS_VERIFY" ]; then
+    global_args="$global_args --insecure-skip-tls-verify=\"$WERCKER_HELM_INSECURE_SKIP_TLS_VERIFY\""
+  fi
+
+  # certificate-authority
+  if [ -n "$WERCKER_HELM_CERTIFICATE_AUTHORITY" ]; then
+    global_args="$global_args --certificate-authority=\"$WERCKER_HELM_CERTIFICATE_AUTHORITY\""
+  fi
+  
+  # client-certificate
+  if [ -n "$WERCKER_HELM_CLIENT_CERTIFICATE" ]; then
+    global_args="$global_args --client-certificate=\"$WERCKER_HELM_CLIENT_CERTIFICATE\""
+  fi
+  
+  # client-key
+  if [ -n "$WERCKER_HELM_CLIENT_KEY" ]; then
+    global_args="$global_args --client-key=\"$WERCKER_HELM_CLIENT_KEY\""
+  fi
 
   # token
   #if [ -n "$WERCKER_HELM_CHART_GENERATE_CHART_GENERATE_TOKEN" ]; then
@@ -233,8 +286,9 @@ main() {
   #info "executing chartify command"
   #$chartify $chartify_cmd $chartify_args
   
-  echo  eval "$kubectl" "$global_args" "$raw_global_args" "$cmd" "$args" "$raw_args"
-  eval "$kubectl" "$global_args" "$raw_global_args" "$cmd" "$args" "$raw_args"
+  info "Executing Helm Command"
+  echo "$helm" "$helm_cmd" "$helm_args" 
+  eval "$helm" "$helm_cmd" "$helm_args" 
 }
 
 main;
